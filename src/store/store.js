318f7e7e2f067ed531e2fea084579b7886ca4693
Ali Hassaine <ali.hs@outlook.fr>
2023-03-27 16:48:01 +0000

    Configure RTK Query

--- src/store/store.js
+++ src/store/store.js
@@ -2,10 +2,15 @@ import { configureStore } from '@reduxjs/toolkit';
 import taskReducer from '../pages/board/task/taskSlice';
 import boardReducer from '../pages/board/boardSlice';
 import labelReducer from '../pages/board/labels/labelsSlice';
+import { apiSlice } from 'api/ApiSlice';
 export default configureStore({
   reducer: {
     tasks: taskReducer,
     boards: boardReducer,
     labels: labelReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    getDefaultMiddleware().concat(apiSlice.middleware);
   },
 });
